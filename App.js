55ca8d4599951cc63fb4f778cbd3a5acc6a6fa75
macbook <harisgorcevic1000@gmail.com>
2024-03-25 23:07:28 +0100

    working

--- App.js
+++ App.js
@@ -8,22 +8,28 @@ import {
   Modal,
   Button,
 } from "react-native";
-const logo = require("./assets/adaptive-icon.png");
+import logo from "./assets/adaptive-icon.png";
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+    <View style={{ flex: 1, backgroundColor: "blue", padding: 60 }}>
+      <Image style={{ width: 200, height: 300 }} source={logo}></Image>
       <Button
         title="Press"
-        color={"midnightblue"}
+        color={"white"}
         onPress={() => setIsModalVisible(true)}
       />
-      <Modal visible={isModalVisible}>
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Text>Modal Button</Text>
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
+        <View style={{ flex: 1, backgroundColor: "red", padding: 60 }}>
+          <Text style={{ color: "white" }}>Modal Button</Text>
           <Button
             title="Close"
-            color={"midnightblue"}
+            color={"white"}
             onPress={() => setIsModalVisible(false)}
           />
         </View>
