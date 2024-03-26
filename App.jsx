eb076bf5bfd636b2df3df8f78d6a91e406f9c5f4
macbook <harisgorcevic1000@gmail.com>
2024-03-26 23:47:04 +0100

    Work

--- App.jsx
+++ App.jsx
@@ -10,15 +10,20 @@ import {
   StatusBar,
   ActivityIndicator,
   Alert,
+  StyleSheet
 } from "react-native";
 import Greet from "./components/Greet";
 import logo from "./assets/adaptive-icon.png";
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <View style={{ backgroundColor: "blue", padding: 60, height: "100%" }}>
-      <Greet name="Bruce Wayne" />
-      <Greet name="Bruce cler" />
+    <View style={style.container}>
+      <Text>asda</Text>
     </View>
   );
 }
+const style = StyleSheet.create({
+  container:{
+    backgroundColor: "red", padding: 60, height: "100%" 
+  }
+})
