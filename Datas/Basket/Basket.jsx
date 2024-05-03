ae849627bec434910215849460decfa0ea0d397c
macbook <harisgorcevic1000@gmail.com>
2024-05-03 21:20:43 +0200

    Stylzing

--- Datas/Basket/Basket.jsx
+++ Datas/Basket/Basket.jsx
@@ -9,11 +9,11 @@ import {
 } from "react-native";
 import { CustomContext } from "../../Context/ContextProvider";
 import BasketData from "../../components/BasketData";
-
 export default function Basket() {
   const { sendDataFunction, setTotalPrice, totalPrice, setSendDataFunction } =
     useContext(CustomContext);
   const [showBill, setShowBill] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
   useEffect(() => {
     setTotalPrice(sendDataFunction.reduce((acc, curr) => acc + curr.price, 0));
   }, []);
@@ -26,8 +26,26 @@ export default function Basket() {
     </View>
   );
 
+  const handlePay = () => {
+    setShowBill(false);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2500);
+  };
   return (
     <View style={styles.container}>
+      {showAlert && (
+        <View style={styles.alert}>
+          <Text
+            style={styles.closebtn}
+            class="closebtn"
+            onclick="this.parentElement.style.display='none';"
+          >
+            Completed Paying Up
+          </Text>
+        </View>
+      )}
       <FlatList
         numColumns={windowWidth > 700 ? 2 : 1}
         data={sendDataFunction}
@@ -64,10 +82,7 @@ export default function Basket() {
           <Text style={{ color: "white" }}>
             Total Price: {totalPrice.toFixed(2)}$
           </Text>
-          <TouchableOpacity
-            onPress={() => setShowBill(false)}
-            style={styles.pay}
-          >
+          <TouchableOpacity onPress={handlePay} style={styles.pay}>
             <Text
               style={{
                 color: siva,
@@ -77,7 +92,10 @@ export default function Basket() {
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => setShowBill(false)}
+            onPress={() => {
+              setShowBill(false);
+              setShowAlert(true);
+            }}
             style={styles.exit}
           >
             <Text
@@ -146,5 +164,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 10,
+  },
+  alert: {
+    padding: 20,
+    backgroundColor: "lightgreen",
+    color: "white",
+    position: "absolute",
+    top: 20,
+    left: 75,
+    width: 300,
+    zIndex: 10,
+  },
+  closebtn: {
+    marginleft: 15,
+    color: "white",
+    fontSize: 22,
+    lineHeight: 20,
+    alignSelf: "center",
   },
 });
